e16f159d6d99c9ca4cc1a77169b902fa6a1b19d4
Xavier <mesas.dev@gmail.com>
2018-05-17 17:57:09 +0200

    Connexion y includes

---
+++ TIENDA/Connections/conexion.php
@@ -0,0 +1,21 @@
+<?php
+
+
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+$hostname_conn = "p:localhost";
+$database_conn = "persephone";
+$username_conn = "root";
+$password_conn = "";
+$conn = mysqli_connect($hostname_conn, $username_conn, $password_conn, $database_conn);
+mysqli_set_charset($conn, 'utf8');
+
+if (is_file("includes/funciones.php")) 
+include("includes/funciones.php"); 
+else
+{
+	include("../includes/funciones.php");
+}
+?>
