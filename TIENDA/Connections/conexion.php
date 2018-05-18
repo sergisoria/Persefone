ffc69a26c83abd2e19aa666d473fd5f04e48916c
Unknown <oriolvalnun@monlau.es>
2018-05-18 16:30:55 +0200

    Conexion hecha

--- TIENDA/Connections/conexion.php
+++ TIENDA/Connections/conexion.php
@@ -7,20 +7,21 @@ if (!isset($_SESSION)) {
 
 $hostname_conn = "172.17.129.142";
 $database_conn = "persephone";
-$username_conn = "root";
-$password_conn = "";
+$username_conn = "persephone";
+$password_conn = "persephone";
 $conn = mysqli_connect($hostname_conn, $username_conn, $password_conn, $database_conn);
 mysqli_set_charset($conn, 'utf8');
 
 
 if (is_file("includes/funciones.php")) 
-include("includes/funciones.php"); 
 
 
+echo "ok";
 
-else
+	
 {
-
-	include("../../includes/funciones.php");
+echo 'error';
+	
 }
 ?>
+	
