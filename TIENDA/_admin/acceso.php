45a3fb1ce9c92e9d469b4a718be2f0ac80e4a3fb
Xavier <mesas.dev@gmail.com>
2018-05-22 19:54:30 +0200

    19:54 22/05/2018

--- TIENDA/_admin/acceso.php
+++ TIENDA/_admin/acceso.php
@@ -17,7 +17,7 @@ function mysqli_result($res, $row, $field=0) {
 }
 
 if (isset($_POST['email'])) {
-  $loginUsername=$_POST['email'];
+  $loginUsername=$_POST['email'];	
   //ATENCIÓN USAMOS MD5 para guardar la contraseña.
   $password=md5($_POST['password']);
   $MM_fldUserAuthorization = "Rol";
@@ -26,8 +26,9 @@ if (isset($_POST['email'])) {
   $MM_redirecttoReferrer = false;
   
   	
-  $LoginRS__query=sprintf("SELECT idUsuario, Correo, password, Rol FROM Usuarios WHERE Correo=%s AND Password=%s AND Rol=1",
-  GetSQLValueString($loginUsername, "text"), GetSQLValueString($password, "text")); 
+  $LoginRS__query=sprintf("SELECT idUsuario, Correo, Password, Rol FROM Usuarios WHERE Correo=%s AND Password=%s AND Rol=1",
+  GetSQLValueString($loginUsername, "text"),
+GetSQLValueString($password, "text")); 
    
   $LoginRS = mysqli_query($conn,  $LoginRS__query) or die(mysqli_error($conn));
   $loginFoundUser = mysqli_num_rows($LoginRS);
@@ -39,9 +40,9 @@ if (isset($_POST['email'])) {
     //declare two session variables and assign them
     $_SESSION['MM_Username'] = $loginUsername;
     $_SESSION['MM_UserGroup'] = $loginStrGroup;	 
-    $_SESSION['NOMBREWEB_UserId'] = mysqli_result($LoginRS,0,'idUsuario');
-    $_SESSION['NOMBREWEB_Mail'] = mysqli_result($LoginRS,0,'email');
-    $_SESSION['NOMBREWEB_Nivel'] = mysqli_result($LoginRS,0,'Rol');
+    $_SESSION['persefone_Login'] = mysqli_result($LoginRS,0,'Login');
+    $_SESSION['persefone_Mail'] = mysqli_result($LoginRS,0,'Correo');
+    $_SESSION['persefone_Nivel'] = mysqli_result($LoginRS,0,'Rol');
 	//ContabilizarAcceso($_SESSION['NOMBREWEB_UserId']);
 	
 	/* DESCOMENTAR SOLO SI SE USA EL CHECK DE RECORDAR CONTRASEÑA, HABRÁ QUE USAR LA FUNCIÓN generar_cookie()
--- TIENDA/_admin/index.php
+++ TIENDA/_admin/index.php
@@ -45,7 +45,7 @@
                         <form role="form" action="acceso.php" method="post">
                             <fieldset>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="email" name="email" >
+                                    <input class="form-control" placeholder="email" name="email" type="email" >
                                 </div>
                                 <div class="form-group">
                                     <input class="form-control" placeholder="Contraseña" name="password" type="password" value="">
