16e4ea0f67b7429476315ec71ed30f40bc3a47a8
Unknown <alexcasah@gmail.com>
2018-05-28 15:29:00 +0200

    15:28 28/05/2018

--- DataBase/PHPMYADMIN/DB/persephone.sql
+++ DataBase/PHPMYADMIN/DB/persephone.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-05-2018 a las 19:51:50
+-- Tiempo de generación: 28-05-2018 a las 15:27:34
 -- Versión del servidor: 10.1.31-MariaDB
 -- Versión de PHP: 7.2.4
 
