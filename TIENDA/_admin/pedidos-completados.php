cf3f313c54ec7c61a25cff05de54b9ad2109bdd5
Xavier <mesas.dev@gmail.com>
2018-05-29 18:13:21 +0200

    Auto stash before merge of "master" and "origin/master"
    18:13

---
+++ TIENDA/_admin/pedidos-completados.php
@@ -0,0 +1,136 @@
+<?php require_once('../Connections/conexion.php'); ?>
+<?php require_once('../includes/funciones.php'); ?>
+
+<?php
+
+$variable_Consulta = "0";
+if (isset($VARIABLE)) {
+  $variable_Consulta = $VARIABLE;
+}
+
+$query_DatosConsulta = sprintf("SELECT * FROM pedido Where Estado = 'Completado'");
+$DatosConsulta = mysqli_query($conn,  $query_DatosConsulta) or die(mysqli_error($conn));
+$row_DatosConsulta = mysqli_fetch_assoc($DatosConsulta);
+$totalRows_DatosConsulta = mysqli_num_rows($DatosConsulta);
+
+
+?>
+             
+
+<!DOCTYPE html>
+<html lang="en"><!-- InstanceBegin template="/Templates/Administracion.dwt.php" codeOutsideHTMLIsLocked="false" -->
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- InstanceBeginEditable name="doctitle" -->
+    <title>Pedidos Pendientes</title>
+    <!-- InstanceEndEditable -->
+    <!-- Bootstrap Core CSS -->
+    <?php include("../includes/adm-cabecera.php"); ?>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+</head>
+
+<body>
+<!-- InstanceBeginEditable name="ContenidoAdmin" -->
+<div id="wrapper">
+  <!-- Navigation -->
+  <?php include("../includes/adm-menu.php"); ?>
+  <div id="page-wrapper">
+     <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Pedidos Pendientes</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+	  <a href="usuario-add.php"class="btn btn-outline btn-primary">Añadir Usuario</a><br></br>
+            
+<div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Resultado
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                               <?php if ($totalRows_DatosConsulta > 0) {  ?>
+                                <table class="table table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>IdPedido</th>
+                                            <th>IdUsuario</th>
+                                            <th>FechaInicio</th>
+                                            <th>FechaEstimada</th>
+                                            <th>Estado</th>
+                                            <th>Accion</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                       <?php 
+		//AQUI ES DONDE SE SACAN LOS DATOS, SE COMPRUEBA QUE HAY RESULTADOS
+		
+			 do { 
+              		?>
+              		
+				<tr>
+						<td><?php echo $row_DatosConsulta["idPedido"];?></td>
+						<td><?php echo $row_DatosConsulta["idUsuario"];?></td>
+						<td><?php echo $row_DatosConsulta["FechaInicio"];?></td>
+						<td><?php echo $row_DatosConsulta["FechaEstimada"];?></td>
+						<td><?php echo $row_DatosConsulta["Estado"];?></td>
+			
+						
+				</tr>
+              		
+              		<?php
+              		 } while ($row_DatosConsulta = mysqli_fetch_assoc($DatosConsulta)); 
+	?>
+   
+                                    </tbody>
+                                </table>
+                          <?      
+                                 } 
+		else
+		 { //MOSTRAR SI NO HAY RESULTADOS ?>
+                No hay resultados.
+                <?php } ?>
+                            </div>
+                            <!-- /.table-responsive -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+                
+                <!-- /.col-lg-6 -->
+            </div>
+  </div>
+  <!-- /#page-wrapper -->
+</div>
+<!-- InstanceEndEditable --><!-- /#wrapper -->
+
+     <?php include("../includes/adm-pie.php"); ?>
+   
+
+</body>
+
+<!-- InstanceEnd --></html>
+<?php
+//AÑADIR AL FINAL DE LA PÁGINA
+mysqli_free_result($DatosConsulta);
+?>
